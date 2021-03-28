d5c8fa2e6365d641976a840ec579f569b4a7dc9d
unknown <gaspartdi@gmail.com>
2021-03-27 23:06:28 -0300

    Started with MongoDB

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require("colors");
 const bootcamps = require("./routes/bootcamps");
 
 dotenv.config({ path: "./config/config.env" });
+connectDB();
 const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use(express.json());
 app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});
